9d3eed8e0a22a00692653458b0b79ee0f00dadfa
Jdallos <joshdallos@gmail.com>
2023-11-09 12:37:34 +0200

    Contact support functionality setup

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   // PUBLIC ENV VARIABLE DEFINITIONS HERE
   readonly PUBLIC_VIDEO_URL: string;
   readonly PUBLIC_SUBTITLES_URL: string;
+  readonly PUBLIC_SUPPORT_EMAIL: string
 }
 
 interface ImportMeta {
