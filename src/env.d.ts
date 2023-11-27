b9a2f7afd122bc4a2709204b4e9d49e5391c5f66
Jdallos <joshdallos@gmail.com>
2023-11-27 10:58:13 +0200

    Backup video format added

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,9 @@
 
 interface ImportMetaEnv {
   // PUBLIC ENV VARIABLE DEFINITIONS HERE
-  readonly PUBLIC_VIDEO_URL: string;
+  readonly PUBLIC_VIDEO_URL_WEBM: string;
+  readonly PUBLIC_VIDEO_URL_MP4: string;
+  readonly PUBLIC_VIDEO_PLACEHOLDER: string;
   readonly PUBLIC_SUBTITLES_URL: string;
   readonly PUBLIC_SUPPORT_EMAIL: string
 }
